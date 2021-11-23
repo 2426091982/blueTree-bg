4f52d4c4b6ef90c1f4c63766dc721e117da5ac28
2426091982 <2426091982@qq.com>
2021-11-23 15:33:43 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,79 @@
 import { useCssModule } from "@vue/runtime-dom";
 import HelloWorld from "./components/home.vue";
 let style = useCssModule();
+let collapsed = false;
 </script>
 
 <template>
-  <router-view></router-view>
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model="collapsed" collapsible>
+      <div class="logo">蓝树林企业后台</div>
+      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline">
+        <a-menu-item key="1">
+          <i class="iconfont icon-zhuye"></i>
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <i class="iconfont icon-zhuye"></i>
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span slot="title">
+              <i class="iconfont icon-zhuye"></i>
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3"> Tom </a-menu-item>
+          <a-menu-item key="4"> Bill </a-menu-item>
+          <a-menu-item key="5"> Alex </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span slot="title">
+              <i class="iconfont icon-zhuye"></i>
+              <span>User</span>
+            </span>
+          </template>
+          <a-menu-item key="3"> Tom </a-menu-item>
+          <a-menu-item key="4"> Bill </a-menu-item>
+          <a-menu-item key="5"> Alex </a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <i class="iconfont icon-zhuye"></i>
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>User</a-breadcrumb-item>
+          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div
+          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
+        >
+          <router-view></router-view>
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#components-layout-demo-side .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
 }
-
-.title {
-  color: #ddd;
+.logo {
+  font-size: 18px;
+  text-align: center;
+  color: #fff;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 // 引入上面新建的路由文件
 import router from "./router/index";
+import "@/assets/style/iconfont.css";
 let app = createApp(App);
 app.use(router);
 

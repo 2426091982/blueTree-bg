01cb1609804d0306be0e2d28502dd88ebf142574
lxfxy <5407034+lxfxy@user.noreply.gitee.com>
2021-11-23 08:54:26 +0800

    antd-init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { useCssModule } from '@vue/runtime-dom';
 import HelloWorld from './components/HelloWorld.vue'
+let style = useCssModule();
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <a-button>123123123</a-button>
 </template>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,5 +21,9 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.title {
+    color: #ddd;
 }
 </style>
